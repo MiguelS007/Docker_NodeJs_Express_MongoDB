8cd083a880c535069e75ab6657bfa2b680d49001
miguel <miguelsodre007@gmail.com>
2020-12-07 11:43:55 -0300

    rendering messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/comment/comments')
+      .then(res => res.json())
+      .then(resJson => setComments({ comments: resJson }));
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <p>
+        <h1>Comments</h1>
+      </p>
+      <ul>
+        {
+          comments.map(comment => {
+            return <li>{ comment.message }</li>
+          })
+        }
+      </ul>
     </div>
   );
 }
